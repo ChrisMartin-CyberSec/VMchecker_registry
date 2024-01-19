02349d040528f2a4f5971de3c70930bf530ab167
Chris Martin <111389653+ChrisMartin-CyberSec@users.noreply.github.com>
2024-01-18 23:23:33 -0500

    Update regcheck.cpp

--- regcheck.cpp
+++ regcheck.cpp
@@ -1,18 +1,15 @@
 #include "regcheck.h"
 
 
-bool __regstruct::RegKeyCompare(char* regvalue)
+bool __regstruct::GetRegKeyValue()
 {
-
-
-	HKEY hkResult = nullptr;															
+	HKEY hkResult = nullptr;
 	if (RegOpenKeyExA(hKey, lpSubKey, NULL, KEY_READ, &hkResult) != ERROR_SUCCESS)
 	{
 		std::cout << "[-] COULD NOT ACCESS REGISTRY..." << std::endl;
 		return FALSE;
 	}
 
-	char value[1024]{};
 	DWORD sizeof_value = sizeof(value);
 
 	if (RegQueryValueExA(hkResult, lpValueName, NULL, NULL, (LPBYTE)value, &sizeof_value) != ERROR_SUCCESS)
@@ -21,7 +18,23 @@ bool __regstruct::RegKeyCompare(char* regvalue)
 		return FALSE;
 	}
 
-	strcpy_s(regvalue, sizeof(value), value);
+	std::cout << "\t[+] REGISTRY VALUE: " << value << std::endl;
 
 	return TRUE;
+}
+
+void __regstruct::isVM()
+{
+	for (int i = 0; i < vendorlistsize; i++)
+	{
+		if (strstr(value, vendors[i]))
+		{
+			inVM = TRUE;
+			std::cout << "\t[-] PROBABLY IN A VM..." << std::endl << std::endl;
+		}
+	}
+	if (!inVM)
+	{
+		std::cout << "\t[+] PROBABLY NOT IN A VM!" << std::endl << std::endl;
+	}
 }
